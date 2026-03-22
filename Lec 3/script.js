4b031ef9ea3473cea85123df457f4b6055c6e3eb
AhmarSaeed1011 <ahmarsaeed1011@gmail.com>
2026-03-23 00:13:46 +0500

    first commit

---
+++ Lec 3/script.js
@@ -0,0 +1,64 @@
+// for( initialization, condition, update ) {
+//     // code to be executed
+// }
+
+// for(let i = 1; i <= 5; i++) {
+//     console.log(i);
+// }
+
+// let value = 0;
+// for(let i = 1; i <= 5; i++) {
+//     value += i;
+// }
+// console.log(value); // 15
+
+// for(let i = 1; i <=35; i++){
+//     console.log(i*3);
+// }
+
+// for(let i = 1; i <=10; i++){
+//     console.log(`3 * ${i} = ${i*3}`);
+// }
+
+
+// whileloop
+
+// while(condition) {
+//     // code to be executed
+// }
+// let i = 1;
+// while(i <= 5) {
+//     console.log(i);
+//     i++;
+// }
+
+// whilie loop state driven
+
+//  
+
+// let i = 1;
+// do {
+//     console.log("Playing");
+//     i++;
+// }while(i <= 5)
+
+// for(let i = 1; i <= 5; i++) {
+//     if(i == 5) {
+//         break; // exit the loop
+//     }
+//    console.log(i); // 2
+// }
+
+// for(let i = 1; i <= 5; i++) {
+//     if(i == 3) {
+//         continue; // skip the current iteration
+//     }
+//    console.log(i); // 2
+// }
+
+// nested loop
+// for(let i = 1; i <= 2; i++) {
+//     for(let j = 1; j <= 4; j++) {
+//         console.log(j);
+//     }
+// }
